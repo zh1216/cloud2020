b1c3a229399c65deb4d4dd143c74e4c7970768cb
zh1216 <15612425115@163.com>
2020-04-23 22:11:27 +0800

    增加Spring cloud Config 配置中心

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ *@project:
+ *@description:
+ *@author:zhangwenqiang
+ *@date: 2020/4/23
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigCenterMain3344.class, args);
+	}
+}
