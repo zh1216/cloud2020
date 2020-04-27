adc2851c1d9e935e10f012a7fe3c3ec0f36d3b48
zh1216 <15612425115@163.com>
2020-04-27 21:08:10 +0800

    配置消费失败相关处理

--- cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/controller/TestController.java
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/controller/TestController.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.didi.TestTopic;
 import com.netflix.discovery.converters.Auto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.messaging.support.MessageBuilder;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  *@date: 2020/4/27
  */
 @RestController
+@Slf4j
 @EnableBinding(TestTopic.class)
 public class TestController {
 
@@ -24,7 +26,7 @@ public class TestController {
 
 	@GetMapping("/sendMessage")
 	public String messageWithMQ(@RequestParam String message) {
-		System.out.println("发送消息" + message);
+		log.info("发送消息{}", message);
 		testTopic.output().send(MessageBuilder.withPayload(message).build());
 		return "ok";
 	}
--- cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/didi/TestListener.java
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/atguigu/springcloud/didi/TestListener.java
@@ -1,7 +1,9 @@
 package com.atguigu.springcloud.didi;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
@@ -15,8 +17,30 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class TestListener {
 
+	int counter = 1;
+
 	@StreamListener(TestTopic.INPUT)
-	public void receive(Message message){
-		log.info("接收到消息{}",message.getPayload().toString());
+	public void receive(Message message) {
+		log.info("接收到消息{}", message.getPayload().toString());
+		if (counter == 3) {
+			counter = 1;
+			// 抛出此异常，直接进入死信队列
+			throw new AmqpRejectAndDontRequeueException("tried 3 times failed，send to dlq！");
+		} else {
+			counter++;
+			// 模拟消息消费失败
+			throw new RuntimeException("Message consumer failed!");
+		}
 	}
+
+//	/**
+//	 * 	消息消费方法失败，调用这个方法
+//	 * 	aaa-topic : 对应输入通道的destination(目标)
+//	 * 	stream-exception-handler: 对应输入通道组名
+//	 * @param message
+//	 */
+//	@ServiceActivator(inputChannel="aaa-topic.stream-exception-handler.errors")
+//	public void errorMethod(Message message){
+//		log.info("message consume failed , call back ！");
+//	}
 }
