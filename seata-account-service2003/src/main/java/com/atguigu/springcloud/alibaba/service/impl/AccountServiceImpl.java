d08f6150a59cfe501ffcc9b75576273d0f75ffa0
zh1216 <15612425115@163.com>
2020-05-28 19:39:46 +0800

    断点调试

--- seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/service/impl/AccountServiceImpl.java
+++ seata-account-service2003/src/main/java/com/atguigu/springcloud/alibaba/service/impl/AccountServiceImpl.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud.alibaba.service.impl;
 
 import com.atguigu.springcloud.alibaba.dao.AccountDao;
 import com.atguigu.springcloud.alibaba.service.AccountService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -15,6 +16,7 @@ import java.util.concurrent.TimeUnit;
  *@date: 2020/5/19
  */
 @Service
+@Slf4j
 public class AccountServiceImpl implements AccountService {
 
 	@Resource
@@ -23,8 +25,9 @@ public class AccountServiceImpl implements AccountService {
 	@Override
 	public void decrease(Long userId, BigDecimal money) {
 		// 模拟超时
-		try { TimeUnit.SECONDS.sleep(20);} catch (InterruptedException e) {e.printStackTrace();}
+		//try { TimeUnit.SECONDS.sleep(20);} catch (InterruptedException e) {e.printStackTrace();}
 		accountDao.decrease(userId,money);
+		log.info("account service 扣减余额结束");//断点
 	}
 }
 
